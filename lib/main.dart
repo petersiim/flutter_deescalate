59cb829d380b0f87661b022e31ddefa24766c121
Simon Peter <petersim@student.ethz.ch>
2024-04-09 08:36:19 +0200

    dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
   runApp(const MyApp());
